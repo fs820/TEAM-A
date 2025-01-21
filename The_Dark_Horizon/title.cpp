55852dce5ead3d64b4acda176177b27f04425627
髙橋美咲(takahashi misaki) <241311@st.yoshida-g.ac.jp>
2025-01-21 11:06:43 +0900

    title.cpp 編集中

--- The_Dark_Horizon/title.cpp
+++ The_Dark_Horizon/title.cpp
@@ -9,11 +9,91 @@
 #include"input.h"
 #include"fade.h"
 
+// マクロ定義
+#define MAX_TEXTUREW_TITLE (1280.0f)		// タイトルテクスチャの横幅
+#define MAX_TEXTUREH_TITLE (720.0f)			// タイトルテクスチャの縦幅
+#define MAX_TEXTUREW_TITLENAME (1000.0f)	// タイトル名テクスチャの横幅
+#define MAX_TEXTUREH_TITLENAME (200.0f)		// タイトル名テクスチャの縦幅
+#define MAX_TEXTUREW_TITLEUI (500.0f)		// タイトルUIテクスチャの横幅
+#define MAX_TEXTUREH_TITLEUI (90.0f)		// タイトルUIテクスチャの縦幅
+#define MAX_TEXTUREW (10.0f)				// UIテクスチャ同士の横幅
+#define MAX_TITLEUI (3)						// UIテクスチャの最大数
+
+// グローバル変数
+LPDIRECT3DTEXTURE9 g_pTextureTitle[TITLETYPE_MAX] = {};	// テクスチャへのポインタ
+LPDIRECT3DVERTEXBUFFER9 g_pVtxBuffTitle = NULL;			// 頂点バッファへのポインタ
+TitleUI g_TitleUI[MAX_TITLEUI];							// タイトルの構造体
+
 //--------------------
 //初期化処理
 //--------------------
 void InitTitle(void)
 {
+	LPDIRECT3DDEVICE9 pDevice;  // デバイスへのポインタ
+
+	// タイトルの構造体の初期化
+	for (int nCount = 0; nCount < MAX_TITLEUI; nCount++)
+	{
+		g_TitleUI[nCount].pos = D3DXVECTOR3(640.0f, 400.0f, 0.0f);	// 位置を初期化する
+		g_TitleUI[nCount].fWidth = MAX_TEXTUREW_TITLEUI;			// 横幅を初期化する 
+		g_TitleUI[nCount].fHeght = MAX_TEXTUREH_TITLEUI;			// 縦幅を初期化する 
+		g_TitleUI[nCount].bUse = false;								// 使用しているかを初期化する 
+		g_TitleUI[nCount].nCountTime = 0;							// カウントを初期化する 
+	}
+
+	// デバイスの取得
+	pDevice = GetDevice();
+
+	for (int nCount = 0; nCount < TITLETYPE_MAX; nCount++)
+	{
+		// テクスチャの読み込み
+		D3DXCreateTextureFromFile(pDevice,
+			ITEM_TEXTURE[nCount],
+			&g_pTextureTitle[nCount]);
+	}
+
+	// 頂点バッファの生成
+	pDevice->CreateVertexBuffer(sizeof(VERTEX_2D) * 4 * (MAX_TITLEUI + 2),
+		D3DUSAGE_WRITEONLY,
+		FVF_VERTEX_2D,
+		D3DPOOL_MANAGED,
+		&g_pVtxBuffTitle,
+		NULL);
+
+	VERTEX_2D* pVtx;  // 頂点情報へのポインタ
+
+	// 頂点バッファをロックし、頂点情報へのポインタを取得
+	g_pVtxBuffTitle->Lock(0, 0, (void**)&pVtx, 0);
+
+	for (int nCount = 0; nCount < TITLETYPE_MAX; nCount++)
+	{
+		// 頂点座標の設定
+		pVtx[0 + (4 * nCount)].pos = D3DXVECTOR3(0.0f, 0.0f, 0.0f);
+		pVtx[1 + (4 * nCount)].pos = D3DXVECTOR3(0.0f, 0.0f, 0.0f);
+		pVtx[2 + (4 * nCount)].pos = D3DXVECTOR3(0.0f, 0.0f, 0.0f);
+		pVtx[3 + (4 * nCount)].pos = D3DXVECTOR3(0.0f, 0.0f, 0.0f);
+
+		// rhwの設定																					
+		pVtx[0 + (4 * nCount)].rhw = 1.0f;
+		pVtx[1 + (4 * nCount)].rhw = 1.0f;
+		pVtx[2 + (4 * nCount)].rhw = 1.0f;
+		pVtx[3 + (4 * nCount)].rhw = 1.0f;
+
+		// 頂点カラーの設定
+		pVtx[0 + (4 * nCount)].col = D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f);
+		pVtx[1 + (4 * nCount)].col = D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f);
+		pVtx[2 + (4 * nCount)].col = D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f);
+		pVtx[3 + (4 * nCount)].col = D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f);
+
+		// テクスチャ座標の設定
+		pVtx[0 + (4 * nCount)].tex = D3DXVECTOR2(0.0f, 0.0f);
+		pVtx[1 + (4 * nCount)].tex = D3DXVECTOR2(1.0f, 0.0f);
+		pVtx[2 + (4 * nCount)].tex = D3DXVECTOR2(0.0f, 1.0f);
+		pVtx[3 + (4 * nCount)].tex = D3DXVECTOR2(1.0f, 1.0f);
+	}
+
+	// 頂点バッファをアンロックする
+	g_pVtxBuffTitle->Unlock();
 
 }
 
--- The_Dark_Horizon/title.h
+++ The_Dark_Horizon/title.h
@@ -15,6 +15,51 @@
 #define TEXTURE_START "data\\TEXTURE\\Enter or Start.png"
 #define TEXTURE_SELECT "data\\TEXTURE\\Select.png"
 
+// タイトルUIの列挙型
+typedef enum
+{
+	UITYPE_GAMEPLAY = 0,
+	UITYPE_VERSUS,
+	UITYPE_TUTORIAL,
+	UITYPE_RANKING,
+	UITYPE_OPTION,
+	UITYPE_EXIT,
+	UITYPE_MAX
+}UIType;
+
+// タイトルの列挙型
+typedef enum
+{
+	TITLETYPE_SKY = 0,
+	TITLETYPE_MOON,
+	TITLETYPE_NAME,
+	TITLETYPE_GAMEPLAY,
+	TITLETYPE_TUTORIAL,
+	TITLETYPE_EXIT,
+	TITLETYPE_MAX,
+}TitleType;
+
+// 列挙型にテクスチャ名を代入
+static const char* ITEM_TEXTURE[TITLETYPE_MAX] =
+{
+	"data\\TEXTURE\\titlesky000.jpg",
+	"data\\TEXTURE\\titlemoon000.png",
+	"data\\TEXTURE\\titlename000.png",
+	"data\\TEXTURE\\Select.png",
+	"data\\TEXTURE\\Select.png",
+	"data\\TEXTURE\\Select.png",
+};
+
+// タイトルUIの構造体
+typedef struct
+{
+	D3DXVECTOR3 pos;		// 位置
+	float fWidth;			// 横幅
+	float fHeght;			// 縦幅
+	bool bUse;				// 使用しているかどうか
+	int nCountTime;			// 時間をカウントする
+}TitleUI;
+
 void InitTitle(void);//タイトル初期化処理
 void UninitTitle(void);//タイトル終了処理
 void UpdateTitle(void);//タイトル更新処理
